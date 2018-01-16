59b201c4e04c687ae8b350f6ead5c2a161f823fb
Rakesh Kumar <rakeshgat@gmail.com>
2018-01-16 00:48:30 -0600

    geolocation

--- public/js/index.js
+++ public/js/index.js
@@ -3,13 +3,48 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
+});
+
+socket.on('newMessage', function (msg) {
+    console.log('newMessage:', msg);
+    var li = $('<li></li>');
+    li.text(`${msg.from}: ${msg.text}`);
+    $('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function(locationMsg) {
+    var li = $('<li></li>');
+    var a = $('<a target="_blank">My current location</a>');
+    li.text(`${locationMsg.from}`);
+    a.attr('href', locationMsg.url);
+    li.append(a);
+    $('#messages').append(li);
+});
+
+$('#chat-form').on('submit', (e) => {
+    e.preventDefault();
 
     socket.emit('createMessage', {
-        from: 'Rakesh',
-        text: 'Hello !!'
+        from: 'User',
+        text: $('#message').val()
+    }, (ack) => {
+        console.log('ack');
     });
 });
 
-socket.on('newMessage', function(msg) {
-    console.log('newMessage:', msg);
+var locationButton = $('#send-location');
+
+locationButton.on('click', () => {
+    if(!navigator.geolocation) {
+        return alert('Geolocation not supported by browser');
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position);
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, (err) => {
+      alert('Unable to fetch location');
+    });
 });
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/message');
+const {generateLocationMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -16,14 +18,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New User Connected !');
 
-    socket.on('createMessage', (msg) => {
-        console.log(`Message recieved on server is: ${JSON.stringify(msg)}`);
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat'));
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined') );
+
+    socket.on('createMessage', (msg, callback) => {
+        //console.log(`Message recieved on server is: ${JSON.stringify(msg)}`);
+        io.emit('newMessage', generateMessage(msg.from, msg.text));
+        callback('This is an ack from the server');
     });
 
-    socket.emit('newMessage', {
-        from: 'John',
-        text: 'Hey man !',
-        createdAt: 123
+    socket.on('createLocationMessage', (location) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', location.latitude, location.longitude));
     });
 });
 
